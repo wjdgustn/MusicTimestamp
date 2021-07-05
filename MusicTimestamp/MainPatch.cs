5515e3ded2ba135af00fee879dd24a380aa721bf
Hyunsoo, Chung <admin@hyonsu.com>
2021-07-05 09:41:59 +0900

    Add text shadow

--- MusicTimestamp/MainPatch.cs
+++ MusicTimestamp/MainPatch.cs
@@ -7,6 +7,15 @@ namespace MusicTimestamp.MainPatch {
         public static string Content = "";
         
         void OnGUI() {
+            if (Main.Settings.TextShadow) {
+                GUIStyle shadow = new GUIStyle();
+                shadow.fontSize = Main.Settings.TextSize;
+                shadow.font = RDString.GetFontDataForLanguage(SystemLanguage.Korean).font;
+                shadow.normal.textColor = Color.black;
+                
+                GUI.Label(new Rect(Main.Settings.PositionX + 12, Main.Settings.PositionY - 8, Screen.width, Screen.height), Content, shadow);
+            }
+
             GUIStyle style = new GUIStyle();
             style.fontSize = Main.Settings.TextSize;
             style.font = RDString.GetFontDataForLanguage(RDString.language).font;
--- MusicTimestamp/Settings.cs
+++ MusicTimestamp/Settings.cs
@@ -3,11 +3,12 @@ using UnityModManagerNet;
 
 namespace MusicTimestamp {
     public class MainSettings : UnityModManager.ModSettings, IDrawable {
+        [Draw("")] public string TextTemplate = "음악 시간 : <NowMinute>:<NowSecond> / <TotalMinute>:<TotalSecond>";
+        [Draw("")] public string NotPlaying = "플레이 중이 아님";
         [Draw("위치 X좌표(Position X)")] public int PositionX = 0;
         [Draw("위치 Y좌표(Position Y)")] public int PositionY = 170;
         [Draw("글자 크기(Font Size)")] public int TextSize = 50;
-        [Draw("")] public string TextTemplate = "음악 시간 : <NowMinute>:<NowSecond> / <TotalMinute>:<TotalSecond>";
-        [Draw("")] public string NotPlaying = "플레이 중이 아님";
+        [Draw("텍스트 그림자")] public bool TextShadow = false;
 
         public override void Save(UnityModManager.ModEntry modEntry) {
             Save(this, modEntry);
